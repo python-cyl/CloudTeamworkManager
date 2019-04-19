f2c101c23258b3408668da19479d485df194dec9
叮咛 宋 <song_dingning@hotmail.com>
2019-04-19 21:47:03 +0800

    添加项目文件。

---
+++ CloudTeamworkManager/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\song_\source\repos\CloudTeamworkManager\CloudTeamworkManager\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ CloudTeamworkManager/notification/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class notificationConfig(AppConfig):
+    name = 'notification'

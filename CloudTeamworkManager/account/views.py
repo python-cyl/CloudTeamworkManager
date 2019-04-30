2fdad2578b39ee30bc16c3a0668501ec923c0241
叮咛 宋 <song_dingning@hotmail.com>
2019-04-30 20:03:41 +0800

    Restful API!

--- CloudTeamworkManager/account/views.py
+++ CloudTeamworkManager/account/views.py
@@ -10,68 +10,121 @@ from .msgcode import sendcode, verifycode
 
 
 def login_page(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect("/account/space/")
-    forms = LoginForm()
-    return render(request, 'login_page.html', {"form": forms})
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return HttpResponseRedirect("/account/space/")
+        forms = LoginForm()
+        return render(request, 'login_page.html', {"form": forms})
+
+    if request.method == "POST":
+        forms = LoginForm(request.POST)
+
+        if forms.is_valid():
+            user = auth.authenticate(request, username = forms.cleaned_data['user_name'], password = forms.cleaned_data['password'])
+            if user:
+                auth.login(request, user)
+                return render(request, 'login_page.html')
+            else:
+                return render(request, 'login_page.html', {"form": forms, "tip": "用户名或密码错误"})
+        else:
+            return render(request, 'login_page.html', {"form": forms, "tip": "输入内容有误"})
 
 def register_page(request):
-    forms = RegisterForm()
-    return render(request, 'register_page.html', {"form": forms})
+    if request.method == "GET":
+        forms = RegisterForm()
+        return render(request, 'register_page.html', {"form": forms})
+
+    if request.method == "POST":
+        forms = RegisterForm(request.POST)
+
+        if forms.is_valid():
+            code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
+            if code:
+                user = User.objects.get(username = forms.cleaned_data['user_name'])
+                phone_number = UserProfile.objects.get(phone_number = forms.cleaned_data['phone_number'])
+                if not user:
+                    if not phone_number:
+                        User.objects.create_user(username = forms.cleaned_data['user_name'], password=forms.cleaned_data['password'])
+                        UserProfile.objects.create(user_id = user.id, phone_number = forms.cleaned_data['phone_number'])
+                        return HttpResponse("200")
+                    else:
+                        return render(request, 'register_page.html', {"form": forms, "tip": "手机号已被注册"})
+                else:
+                    return render(request, 'register_page.html', {"form": forms, "tip": "用户已存在"})
+            else:
+                return render(request, 'register_page.html', {"form": forms, "tip": "短信验证码有误"})
+        else:
+            return render(request, 'register_page.html', {"form": forms, "tip": "输入内容有误"})
 
 def get_password_page(request):
-    forms = GetPasswordForm()
-    return render(request, 'get_password_page.html', {"form": forms})
+    if request.method == "GET":
+        forms = GetPasswordForm()
+        return render(request, 'get_password_page.html', {"form": forms})
 
-def login_submit(request):
-    forms = LoginForm(request.POST)
+        forms = GetPasswordForm(request.POST)
 
-    if forms.is_valid():
-        user = auth.authenticate(request, username = forms.cleaned_data['user_name'], password = forms.cleaned_data['password'])
-        if user:
-            auth.login(request, user)
-            return render(request, 'login_page.html')
-        else:
-            return render(request, 'login_page.html', {"form": forms, "tip": "用户名或密码错误"})
-    else:
-        return render(request, 'login_page.html', {"form": forms, "tip": "输入内容有误"})
-
-def register_submit(request):
-    forms = RegisterForm(request.POST)
-
-    if forms.is_valid():
-        code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
-        if code:
-            user = User.objects.get(username = forms.cleaned_data['user_name'])
-            phone_number = UserProfile.objects.get(phone_number = forms.cleaned_data['phone_number'])
-            if not user:
-                if not phone_number:
-                    User.objects.create_user(username = forms.cleaned_data['user_name'], password=forms.cleaned_data['password'])
-                    UserProfile.objects.create(user_id = user.id, phone_number = forms.cleaned_data['phone_number'])
-                    return HttpResponse("200")
-                else:
-                    return render(request, 'register_page.html', {"form": forms, "tip": "手机号已被注册"})
+    if request.method == "POST":
+        if forms.is_valid():
+            code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
+            if code:
+                user = User.objects.get(username = forms.cleaned_data["user_name"])
+                user.set_password(forms.cleaned_data["password"])
+                user.save()
+                return HttpResponse("200")
             else:
-                return render(request, 'register_page.html', {"form": forms, "tip": "用户已存在"})
+                return render(request, 'get_password.html', {"form": forms, "tip": "短信验证码有误"})
         else:
-            return render(request, 'register_page.html', {"form": forms, "tip": "短信验证码有误"})
-    else:
-        return render(request, 'register_page.html', {"form": forms, "tip": "输入内容有误"})
+            return render(request, 'get_password.html', {"form": forms, "tip": "输入内容有误"})
 
-def get_password_submit(request):
-    forms = GetPasswordForm(request.POST)
+#def login_submit(request):
+#    forms = LoginForm(request.POST)
 
-    if forms.is_valid():
-        code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
-        if code:
-            user = User.objects.get(username = forms.cleaned_data["user_name"])
-            user.set_password(forms.cleaned_data["password"])
-            user.save()
-            return HttpResponse("200")
-        else:
-            return render(request, 'get_password.html', {"form": forms, "tip": "短信验证码有误"})
-    else:
-        return render(request, 'get_password.html', {"form": forms, "tip": "输入内容有误"})
+#    if forms.is_valid():
+#        user = auth.authenticate(request, username = forms.cleaned_data['user_name'], password = forms.cleaned_data['password'])
+#        if user:
+#            auth.login(request, user)
+#            return render(request, 'login_page.html')
+#        else:
+#            return render(request, 'login_page.html', {"form": forms, "tip": "用户名或密码错误"})
+#    else:
+#        return render(request, 'login_page.html', {"form": forms, "tip": "输入内容有误"})
+
+#def register_submit(request):
+#    forms = RegisterForm(request.POST)
+
+#    if forms.is_valid():
+#        code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
+#        if code:
+#            user = User.objects.get(username = forms.cleaned_data['user_name'])
+#            phone_number = UserProfile.objects.get(phone_number = forms.cleaned_data['phone_number'])
+#            if not user:
+#                if not phone_number:
+#                    User.objects.create_user(username = forms.cleaned_data['user_name'], password=forms.cleaned_data['password'])
+#                    UserProfile.objects.create(user_id = user.id, phone_number = forms.cleaned_data['phone_number'])
+#                    return HttpResponse("200")
+#                else:
+#                    return render(request, 'register_page.html', {"form": forms, "tip": "手机号已被注册"})
+#            else:
+#                return render(request, 'register_page.html', {"form": forms, "tip": "用户已存在"})
+#        else:
+#            return render(request, 'register_page.html', {"form": forms, "tip": "短信验证码有误"})
+#    else:
+#        return render(request, 'register_page.html', {"form": forms, "tip": "输入内容有误"})
+
+#def get_password_submit(request):
+#    forms = GetPasswordForm(request.POST)
+
+#    if forms.is_valid():
+#        code = verifycode(forms.cleaned_data['phone_number'], forms.cleaned_data['msgcode'])
+#        if code:
+#            user = User.objects.get(username = forms.cleaned_data["user_name"])
+#            user.set_password(forms.cleaned_data["password"])
+#            user.save()
+#            return HttpResponse("200")
+#        else:
+#            return render(request, 'get_password.html', {"form": forms, "tip": "短信验证码有误"})
+#    else:
+#        return render(request, 'get_password.html', {"form": forms, "tip": "输入内容有误"})
 
 def check_username(request):
     forms = UsernameForm(request.POST)
@@ -133,31 +186,55 @@ def space_page(request):
     if user_info.name:
         return render(request, 'space.html')
     else:
+        return HttpResponseRedirect
+
+@login_required
+def extend_info(request):
+    if request.method == "GET":
         form = extend_info(instance = user_info)
-        return render(request, 'perfect_information.html', {'form': form})
+        return render(request, 'perfect_information.html', {'form': form, "target_url": "/account/perfect_information/"})
+    
+    if request.method == "POST":
+        user_info = UserProfile.objects.get(user_id = request.user.id)
+        form = extend_info(request.POST, instance=user_info)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("200")
+        else:
+            return render(request, 'perfect_information.html', {"form": form, "target_url": "/account/perfect_information/"})
 
-@login_required
-def extend_info_submit(request):
-    user_info = UserProfile.objects.get(user_id = request.user.id)
-    form = extend_info(request.POST, instance=user_info)
-    if form.is_valid():
-        form.save()
-        return HttpResponse("200")
-    else:
-        return render(request, 'perfect_information.html', {"form": form})
+#@login_required
+#def extend_info_submit(request):
+#    user_info = UserProfile.objects.get(user_id = request.user.id)
+#    form = extend_info(request.POST, instance=user_info)
+#    if form.is_valid():
+#        form.save()
+#        return HttpResponse("200")
+#    else:
+#        return render(request, 'perfect_information.html', {"form": form, "target_url": "/account/perfect_information/submit/"})
 
-@login_required
-def change_info_submit(request):
-    user_info = UserProfile.objects.get(user_id = request.user.id)
-    form = change_info(request.POST, instance=user_info)
-    if form.is_valid():
-        form.save()
-        return HttpResponse("200")
-    else:
-        return render(request, 'perfect_information.html', {"form": form})
+#@login_required
+#def change_info_submit(request):
+#    user_info = UserProfile.objects.get(user_id = request.user.id)
+#    form = change_info(request.POST, instance=user_info)
+#    if form.is_valid():
+#        form.save()
+#        return HttpResponse("200")
+#    else:
+#        return render(request, 'perfect_information.html', {"form": form, "target_url": "/account/change_information/submit/"})
 
 @login_required
 def change_info_page(request):
-    user_info = UserProfile.objects.get(user_id = request.user.id)
-    form = change_info(instance=user_info)
-    return render(request, 'perfect_information.html', {'form': form})
+    if request.method == "GET":
+        user_info = UserProfile.objects.get(user_id = request.user.id)
+        form = change_info(instance=user_info)
+        return render(request, 'perfect_information.html', {'form': form, "target_url": "/account/change_information/"})
+
+    if request.method == "POST":
+        user_info = UserProfile.objects.get(user_id = request.user.id)
+        form = change_info(request.POST, instance=user_info)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("200")
+        else:
+            return render(request, 'perfect_information.html', {"form": form, "target_url": "/account/change_information/"})

b60aa655feb65900e69c9615af066a3e9f39b410
叮咛 宋 <song_dingning@hotmail.com>
2019-06-23 10:31:03 +0800

    继续完善了权限部分的功能 优化代码逻辑

--- CloudTeamworkManager/task/models.py
+++ CloudTeamworkManager/task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class task(models.Model):
     task_name = models.CharField(max_length = 20, verbose_name = "任务名")
-    publish_data = models.DateField(auto_now_add=True)
+    publish_data = models.DateField(auto_now_add = True)
     deadline = models.DateField(verbose_name='结束时间')
     task_status = models.IntegerField(verbose_name='任务状态', choices=((0, '已挂起'), (-1, '已结束'), (1, '进行中')))
     members = models.CharField(max_length = 200, verbose_name='成员')
@@ -12,6 +12,8 @@ class task(models.Model):
     task_schedule = models.CharField(max_length = 1000, verbose_name = '时间安排')
     appendixes = models.CharField(max_length = 200, verbose_name = '附件')
     task_progress = models.TextField(verbose_name = '任务进度')
+    creator = models.IntegerField(verbose_name = '创建者')
+    leader = models.CharField(max_length = 20, verbose_name = '组长')
 
     class Meta:
         permissions = {

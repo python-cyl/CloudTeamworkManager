9f89d223c864dc2b3b5049bf68b3a24940563909
叮咛 宋 <song_dingning@hotmail.com>
2019-06-23 10:32:06 +0800

    Revert "继续完善了权限部分的功能"
    This reverts commit b60aa655feb65900e69c9615af066a3e9f39b410.

--- CloudTeamworkManager/task/forms.py
+++ CloudTeamworkManager/task/forms.py
@@ -2,13 +2,13 @@ import re
 from django.forms import ModelForm, ValidationError
 from django.contrib.auth.models import User
 from django import forms
-from .models import task as models_task
+from .models import task
 
 class task(ModelForm):
 
     class Meta:
-        model = models_task
-        exclude = ("creator", "publish_data", "task_schedule", "task_progress", "all_members")
+        model = task
+        exclude = ("publish_data", "task_schedule", "task_progress", "all_members")
 
     def __init__(self, *args, **kwargs):
         super(task, self).__init__(*args, **kwargs)
@@ -17,4 +17,3 @@ class task(ModelForm):
         self.fields["task_description"].required = False
         self.fields["task_need"].required = False
         self.fields["appendixes"].required = False
-        self.fields["leader"].required = False
--- CloudTeamworkManager/task/views.py
+++ CloudTeamworkManager/task/views.py
@@ -1,171 +1,138 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Permission, Group
-from guardian.shortcuts import assign_perm, remove_perm
+from django.contrib.auth.models import User, Permission
+from guardian.shortcuts import assign_perm
 from guardian.decorators import permission_required
-from .models import task as models_task
-from .forms import task as forms_task
+from .models import task
+from .forms import task as form_task
 from account.models import UserProfile
 import re
 import json
 
 
-# position：0，1，分别对应组员，组长
-# 请注意，这个函数不会修改target_user的权限
-def remove_user(target_user, target_task, position = 0):
-    target_user.involved_projects_number -= 1
-    target_user.involved_projects = json.dumps(json.loads(target_user.involved_projects).remove(target_task.id))
-    target_user.save()
-
-# position：0，1，2，分别对应组员，组长, 任务创建者
-def add_user(target_user, target_group, target_task, position = 0):
-    target_user.involved_projects_number += 1
-    target_user.involved_projects = json.dumps(json.loads(user.involved_projects).append(target_task.id))
-    target_user.save()
-
-    user = User.objects.get(id = target_user.user_id)
-    user.groups.add(target_group)
-
-    position == 2 and assign_perm('edit_tasks', target_user, target_task)
+# 检查权限
+def delete_task(request):
+    task.objects.get(id = request.GET.get("task_id")).delete()
+    return HttpResponse("200")
 
 # 检查权限
 def create_task(request):
     if request.method == "GET":
-        return render(request, "create_task.html", {"form": forms_task()})
+        return render(request, "create_task.html", {"form": form_task()})
 
     if request.method == "POST":
-        form = forms_task(request.POST)
+        form = form_task(request.POST)
 
         if form.is_valid():
             # 赋予创建者编辑任务的权限
             form.instance.all_members = form.instance.members
-            form.instance.creator = request.user.id
+            task = form.save()
+            assign_perm('edit_tasks', request.user, task)
+            assign_perm('glance_over_task_details', request.user, task)
+            # 通知新增的同学
 
-            target_task = form.save()
-            group = Group.objects.create(name=str(target_task.id))
-            assign_perm('glance_over_task_details', group, target_task)
-
-            # 配置创建者
-            add_user(UserProfile.objects.get(user_id = request.user.id), group, target_task)
-            # 配置组员
+            # 组内成员参与任务的个数加一，并赋予其浏览该任务的权限
             for i in json.loads(form.instance.members):
                 try:
                     user = UserProfile.objects.get(id = i)
                 except UserProfile.DoesNotExist:
                     continue
-
-                add_user(user, group, target_task)
-
-            # 通知组员
-
+                user.involved_projects_number += 1
+                user.involved_projects = json.dumps(json.loads(user.involved_projects).append(task.id))
+                user.save()
+                user = User.objects.get(id = user.user_id)
+                assign_perm('glance_over_task_details', user, task)
             return HttpResponse("200")
         return HttpResponse("出现错误")
 
 # 检查权限
 def edit_task(request):
     if request.method == 'GET':
-        target_task = models_task.objects.get(id = request.GET.get("task_id"))
-        return render(request, "create_task.html", {"form": forms_task(instance = target_task)})
+        target_task = task.objects.get(id = request.GET.get("task_id"))
+        return render(request, "create_task.html", {"form": form_task(instance = target_task)})
 
     if request.method == "POST":
-        target_task = models_task.objects.get(id = request.POST.get("task_id"))
-        form = forms_task(request.POST, instance = target_task)
+        target_task = task.objects.get(id = request.POST.get("task_id"))
+        form = form_task(request.POST, instance = target_task)
 
         if form.is_valid():
-            # 计算新增成员和被移除的成员
             past_members = set(json.loads(target_task.members))
             current_members = set(json.loads(request.POST.get("members")))
             removed_members = past_members - current_members
             new_members = current_members - past_members
-
-            # 更新参与过该任务的成员列表
             form.instance.all_members = json.dumps(list(set(json.loads(form.instance.all_members)) + current_members))
             form.save()
 
-            # 获取权限组
-            group = Group.objects.get(name=str(target_task.id))
-
-            # 在组中删除该成员，该成员参与过的任务列表更新
             for i in removed_members:
-                try:
-                    user = UserProfile.objects.get(id = i)
-                except UserProfile.DoesNotExist:
-                    continue
+                user = UserProfile.objects.get(id = i)
+                user.involved_projects_number -= 1
+                user.involved_projects = json.dumps(json.loads(user.involved_projects).remove(task.id))
+                user.save()
+                # 这里需要给removed_members发送通知
 
-                remove_user(user, group, target_task)
-                User.objects.get(id = user.user_id).groups.remove(group)
-
-                # 通知组员
-
-            # 配置新增组员
             for i in new_members:
-                try:
-                    user = UserProfile.objects.get(id = i)
-                except UserProfile.DoesNotExist:
-                    continue
-
-                add_user(user, target_task)
-
-                # 通知组员
+                user = UserProfile.objects.get(id = i)
+                user.involved_projects_number += 1
+                user.involved_projects = json.dumps(json.loads(user.involved_projects).append(task.id))
+                user.save()
+                # 这里需要给new_members发送通知
 
             return HttpResponse("200")
         return HttpResponse("出现错误")
 
 # 检查权限
-def delete_task(request):
-    target_task = models_task.objects.get(id = request.GET.get("task_id"))
-    members = json.loads(target_task.members)
-    
-    # 撤销权限，删除记录
-    Group.objects.get(name=str(target_task.id)).delete()
-    remove_perm('edit_tasks', request.user, target_task)
-
-    # 组内成员参与任务的个数减一，删除参与该任务的记录
-    for each in members:
-        try:
-            user = UserProfile.objects.get(id = each)
-        except UserProfile.DoesNotExist:
-            continue
-        
-        remove_user(user, target_task)
-
-    # 通知组内成员
-
-    return HttpResponse("200")
-
-# 检查权限
 def get_members(request):
     members = UserProfile.objects.filter(magor=request.GET.get("key")) | UserProfile.objects.filter(name = request.GET.get("key"))
-    members = members.values("name", "magor", "user_id", "involved_projects_number")
+    members = members.values("name", "magor", "id", "involved_projects_number")
     return HttpResponse(json.dumps(list(members)))
 
 # 检查权限
 def task_page(request):
-    target_task = models_task.objects.get(id = request.GET.get("task_id"))
-    return HttpResponse(target_task)
+    target_task = task.objects.get(id = request.GET.get("task_id"))
+    return HttpResponse(task)
 
 # 检查权限
 def upgrade_process(request):
     # 更新项目进度,修改项目进度
     if request.method == "GET":
-        target_task = models_task.objects.get(id = request.GET.get("task_id"))
+        target_task = task.objects.get(id = request.GET.get("task_id"))
         progress = target_task.task_progress
         return HttpResponse(progress)
 
     if request.method == "POST":
         if request.POST.get("action") == "upgrade":
-            target_task = models_task.objects.get(id = request.POST.get("task_id"))
+            target_task = task.objects.get(id = request.POST.get("task_id"))
             target_task.task_progress = "%s|%s"%(target_task.task_progress, request.POST.get("task_progress"))
-            target_task = forms_task(target_task)
+            target_task = form_task(target_task)
             if target_task.is_valid():
                 target_task.save()
                 return HttpResponse('200')
             return HttpResponse('出现了一些错误!')
         elif request.POST.get("action") == "edit":
-            target_task = models_task.objects.get(id = request.POST.get("task_id"))
+            target_task = task.objects.get(id = request.POST.get("task_id"))
             target_task.task_progress = "%s%s"%(re.match("^.*\|", target_task.task_progress).group(), request.POST.get("task_progress"))
-            target_task = forms_task(target_task)
+            target_task = form_task(target_task)
             if target_task.is_valid():
                 target_task.save()
                 return HttpResponse('200')
             return HttpResponse('出现了一些错误!')
+
+
+#@login_required
+#def get_permission_page(request):
+#    if request.user.has_perm('task.create_tasks'):
+#        return HttpResponse("你已经拥有权限")
+#    permission = Permission.objects.filter(id=42)
+#    request.user.user_permissions.add(permission[0])
+#    return HttpResponse("你没有权限")
+
+#def add_tasks_page(request):
+#    task1 = task.objects.create(publish_data = '1111-10-11', deadline = '1111-10-11', task_need = "这是第一个任务")
+#    task2 = task.objects.create(publish_data = '1111-10-11', deadline = '1111-10-11', task_need = "这是第二个任务")
+#    user = User.objects.get(username = request.user.username)
+#    assign_perm('create_tasks', user, task1)
+#    return HttpResponse("新建成功")
+
+#@permission_required('create_tasks', (task, 'id', "need"))
+#def check_permission_page(request, need):
+#    return HttpResponse("认证成功")

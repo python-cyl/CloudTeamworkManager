b4c922f57feb8d8e2026d90e7032bc93725ae8ee
叮咛 宋 <song_dingning@hotmail.com>
2019-08-08 10:56:17 +0800

    update

--- CloudTeamworkManager/file/urls.py
+++ CloudTeamworkManager/file/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    url(r'^picode/.*$', views.verify_code),
-    url('img_test/', views.show_image),
-    url(r'upload/', views.upload_image),
+    path(r'^picode/.*$', views.verify_code),
+    path('img_test/', views.show_image),
+    path('upload/', views.upload_image),
 ]
